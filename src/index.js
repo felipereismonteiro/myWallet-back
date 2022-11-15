08e07ced3e1330e10b575bc4d9e3eb8360bc1aa5
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-15 10:11:08 -0300

    feat: route sign-in and sign-up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { v4 } from "uuid";
 import joi from "joi";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -14,5 +15,13 @@ let db;
 
 await mongoClient.connect();
 db = mongoClient.db("myWallet");
+
+app.post("/sign-in", async (req, res) => {
+    res.send("Entrou");
+});
+
+app.post("/sign-up", async (req, res) => {
+    res.send("foi sim");
+});
 
 app.listen(5000, () => console.log("Server on port:5000"));
