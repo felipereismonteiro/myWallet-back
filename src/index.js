3ba87b526b6c67440fbd52ec83a82bef6c369e84
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-15 19:08:59 -0300

    feat: route sign-in checking user and creating token

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 import { v4 as uuid } from "uuid";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import Joi from "joi";
 import express from "express";
 import dotenv from "dotenv";
@@ -17,42 +17,69 @@ let db;
 await mongoClient.connect();
 db = mongoClient.db("myWallet");
 
+const dbUsers = db.collection("users");
+
 const signInSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.required(),
 });
 
 const signUpSchema = Joi.object({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().email().required(),
-    password: Joi.required()
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.required(),
 });
 
 app.post("/sign-in", async (req, res) => {
-  res.send("Entrou");
+  const user = req.body;
+
+  try {
+    const { email, password } = await signInSchema.validateAsync(user, {
+      abortEarly: false,
+    });
+    const founded = await dbUsers.findOne({ email });
+
+    if (!founded) {
+      return res.status(404).send("Email not found");
+    } else if (!bcrypt.compareSync(password, founded.password)) {
+      return res.status(401).send("Password doesn`t match");
+    }
+
+    const token = uuid();
+    await db
+      .collection("usersTokens")
+      .insertOne({ id: founded._id, token, lastStatus: Date.now() });
+
+    res.status(200).send("logged");
+  } catch (err) {
+    console.log(err);
+    return res.send(err.details.map((detail) => detail.message));
+  }
 });
 
 app.post("/sign-up", async (req, res) => {
   const user = req.body;
-  const hashPassword = bcrypt.hashSync(req.body.password, 10);
 
   try {
-    const validate = await signUpSchema.validateAsync(user, {abortEarly: false});
-    const userFounded = await db.collection("users").find({$or: [{name: user.name}, {email: user.email}]}).toArray();
+    const validate = await signUpSchema.validateAsync(user, {
+      abortEarly: false,
+    });
+    const userFounded = await db
+      .collection("users")
+      .findOne({ $or: [{ name: user.name }, { email: user.email }] });
 
-    if(userFounded.length > 0) {
+    if (userFounded) {
       return res.status(401).send("Usuario ja cadastrado!");
     }
 
-    await db.collection("users").insertOne({...validate, password: hashPassword})
-
-    res.send("foi sim");
-  } catch(err) {
-    res.send(err.details.map(detail => detail.message));
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+    await db
+      .collection("users")
+      .insertOne({ ...validate, password: hashPassword });
+    res.sendStatus(201);
+  } catch (err) {
+    res.send(err.details.map((detail) => detail.message));
   }
-  
-
-  
 });
 
 app.listen(5000, () => console.log("Server on port:5000"));
