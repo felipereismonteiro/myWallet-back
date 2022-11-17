80246aeb6d68ba27c088cdb2e9dcd0204016c632
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-11-16 21:46:30 -0300

    feat: controllers architecture added.

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,91 @@
+import { v4 as uuid } from "uuid";
+import bcrypt from "bcrypt";
+import { dbUsers, signInSchema, signUpSchema} from "../index.js"
+
+export async function signIn (req, res){
+    const user = req.body;
+  
+    try {
+      const { email, password } = await signInSchema.validateAsync(user, {
+        abortEarly: false,
+      });
+      const founded = await dbUsers.findOne({ email });
+  
+      if (!founded) {
+        return res.status(404).send("Email not found");
+      } else if (!bcrypt.compareSync(password, founded.password)) {
+        return res.status(401).send("Wrong password!!!");
+      }
+  
+      const token = uuid();
+      await db
+        .collection("usersTokens")
+        .insertOne({ id: founded._id, token, lastStatus: Date.now()});
+  
+      res.status(200).send(token);
+    } catch (err) {
+      console.log(err);
+      return res.status(401).send(err.details.map((detail) => detail.message));
+    }
+}
+
+export async function signUp (req, res){
+    const user = req.body;
+  
+    try {
+      const validate = await signUpSchema.validateAsync(user, {
+        abortEarly: false,
+      });
+  
+      const userFounded = await db
+        .collection("users")
+        .findOne({ $or: [{ name: user.name }, { email: user.email }] });
+  
+      if (userFounded) {
+        return res.status(401).send("Usuario ja cadastrado!");
+      } else if (validate.password.length === 0) {
+        return res.status(400).send("Password is needed!!!");
+      }
+  
+      const hashPassword = bcrypt.hashSync(req.body.password, 10);
+      await db
+        .collection("users")
+        .insertOne({ ...validate, password: hashPassword });
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.send(err.details);
+    }
+}
+
+export async function updateToken(req, res){
+    const {lastStatus} = req.body;
+    const {authorization} = req.headers;
+    const token = authorization.replace("Bearer ", "")
+    const founded = await db.collection("usersTokens").findOne({token})
+  
+    console.log(founded);
+  
+    if(!founded) {
+      return res.sendStatus(401);
+    }
+  
+    try {
+      await db.collection("usersTokens").updateOne({token}, {$set: {lastStatus}})
+      res.sendStatus(200);
+    } catch(err) {
+      console.log(err);
+    }
+}
+
+export async function deleteInactiveTokens (){
+    const tokens = await db.collection("usersTokens").find().toArray();
+    const filteredTokens = tokens.filter(t => Date.now() - t.lastStatus >= 10000);
+  
+    try { 
+      filteredTokens.map(t => db.collection("usersTokens").deleteOne({_id: t._id}))
+    } catch(err) {
+      console.log(err);
+    }
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { MongoClient } from "mongodb";
-import { v4 as uuid } from "uuid";
-import bcrypt from "bcrypt";
 import Joi from "joi";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { signIn, signUp, updateToken, deleteInactiveTokens } from "./controllers/authControllers.js"
 dotenv.config();
 
 const app = express();
@@ -17,105 +16,25 @@ let db;
 await mongoClient.connect();
 db = mongoClient.db("myWallet");
 
-const dbUsers = db.collection("users");
+export const dbUsers = db.collection("users");
 
-const signInSchema = Joi.object({
+export const signInSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.required(),
 });
 
-const signUpSchema = Joi.object({
+export const signUpSchema = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
   password: Joi.required(),
 });
 
-app.post("/sign-in", async (req, res) => {
-  const user = req.body;
+app.post("/sign-in", signIn);
 
-  try {
-    const { email, password } = await signInSchema.validateAsync(user, {
-      abortEarly: false,
-    });
-    const founded = await dbUsers.findOne({ email });
+app.post("/sign-up", signUp);
 
-    if (!founded) {
-      return res.status(404).send("Email not found");
-    } else if (!bcrypt.compareSync(password, founded.password)) {
-      return res.status(401).send("Wrong password!!!");
-    }
+app.put("/update", updateToken);
 
-    const token = uuid();
-    await db
-      .collection("usersTokens")
-      .insertOne({ id: founded._id, token, lastStatus: Date.now()});
-
-    res.status(200).send(token);
-  } catch (err) {
-    console.log(err);
-    return res.status(401).send(err.details.map((detail) => detail.message));
-  }
-});
-
-app.post("/sign-up", async (req, res) => {
-  const user = req.body;
-
-  try {
-    const validate = await signUpSchema.validateAsync(user, {
-      abortEarly: false,
-    });
-
-    const userFounded = await db
-      .collection("users")
-      .findOne({ $or: [{ name: user.name }, { email: user.email }] });
-
-    if (userFounded) {
-      return res.status(401).send("Usuario ja cadastrado!");
-    } else if (validate.password.length === 0) {
-      return res.status(400).send("Password is needed!!!");
-    }
-
-    const hashPassword = bcrypt.hashSync(req.body.password, 10);
-    await db
-      .collection("users")
-      .insertOne({ ...validate, password: hashPassword });
-    res.sendStatus(201);
-  } catch (err) {
-    console.log(err);
-    res.send(err.details);
-  }
-});
-
-app.put("/update", async (req, res) => {
-  const {lastStatus} = req.body;
-  const {authorization} = req.headers;
-  const token = authorization.replace("Bearer ", "")
-  const founded = await db.collection("usersTokens").findOne({token})
-
-  console.log(founded);
-
-  if(!founded) {
-    return res.sendStatus(401);
-  }
-
-  try {
-    await db.collection("usersTokens").updateOne({token}, {$set: {lastStatus}})
-    res.sendStatus(200);
-  } catch(err) {
-    console.log(err);
-  }
-});
-
-setInterval(async () => {
-  const tokens = await db.collection("usersTokens").find().toArray();
-  const filteredTokens = tokens.filter(t => Date.now() - t.lastStatus >= 10000);
-
-  try { 
-    filteredTokens.map(t => db.collection("usersTokens").deleteOne({_id: t._id}))
-  } catch(err) {
-    console.log(err);
-  }
-
-}, 15000);
+setInterval(deleteInactiveTokens, 15000);
 
 app.listen(5000, () => console.log("Server on port:5000"));
